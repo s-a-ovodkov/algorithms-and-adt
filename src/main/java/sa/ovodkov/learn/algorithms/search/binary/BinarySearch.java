826fcc44d97d4f1e6b5663245427ffde85083ed8
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2021-05-25 22:06:40 +0300

    Добавлена реализация алгоритма Бинарный поиск

---
+++ src/main/java/sa/ovodkov/learn/algorithms/search/binary/BinarySearch.java
@@ -0,0 +1,36 @@
+package sa.ovodkov.learn.algorithms.search.binary;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Реализация алгоритма "Бинарный поиск"
+ *
+ * @author Ovodkov Sergey created on 25.05.2021
+ */
+public class BinarySearch {
+
+  /**
+   * Поиск индекса первого вхождения указанного элемента в массив
+   *
+   * @param list массив значений среди которых производится поиск
+   * @param targetValue искомое значение
+   * @param <T> тип элемента списка,
+   *            тип элемента должен реализовывать интерфейс {@link Comparable<T>}
+   * @return индекс первого вхождения указанного элемента, если указанного элемента возвращается -1
+   */
+  public static <T extends Comparable<T>> int search(@NotNull List<T> list,
+                                                     @NotNull T targetValue) {
+    int left = 0;
+    int right = list.size() - 1;
+    int guess = (right - left) / 2;
+    while (right >= left) {
+      if (list.get(guess).compareTo(targetValue) == 0) return guess;
+      else if (list.get(guess).compareTo(targetValue) < 0) left = guess + 1;
+      else right = guess - 1;
+      guess = left + ((right - left) / 2);
+    }
+    return -1;
+  }
+}
---
+++ src/test/java/sa/ovodkov/learn/algorithms/search/binary/BinarySearchTest.java
@@ -0,0 +1,68 @@
+package sa.ovodkov.learn.algorithms.search.binary;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Проверка реализации алгоритма бинарный поиск
+ *
+ * @author Ovodkov Sergey created on 25.05.2021
+ */
+@DisplayName("Проверка работы реализации бинарного поиска")
+class BinarySearchTest {
+
+  private final List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+  @ParameterizedTest
+  @CsvSource({
+          "9, 8",   // стандартный случай
+          "1, 0",   // искомый элемент первый в списке
+          "12, 11"  //искомый элемент последний в списке
+  })
+  void searchTest(int targetValue, int expectedResult) {
+      int actualResult = BinarySearch.search(list, targetValue);
+      assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  @DisplayName("Искомого элемента нету в списке")
+  void search4() {
+    int targetValue = 99;
+    int expectedResult = -1;
+
+    int actualResult = BinarySearch.search(list, targetValue);
+
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  @DisplayName("Список состоит из одного элемента")
+  void search5() {
+    List<Integer> smallList = List.of(1, 2);
+    int targetValue = 2;
+    int expectedResult = 1;
+
+    int actualResult = BinarySearch.search(smallList, targetValue);
+
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  @DisplayName("Список состоит из одного элемента")
+  void search6() {
+    List<Integer> emptyList = new ArrayList<>();
+    int targetValue = 99;
+    int expectedResult = -1;
+
+    int actualResult = BinarySearch.search(emptyList, targetValue);
+
+    assertEquals(expectedResult, actualResult);
+  }
+}
